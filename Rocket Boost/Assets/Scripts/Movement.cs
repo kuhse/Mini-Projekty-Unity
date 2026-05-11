095fef6125dca90b8cf89fef3a48c987e3d29022
Sebastian Kucharski <sebastian.kucharski03@gmail.com>
2026-05-11 16:27:27 +0200

    Adding movement and gravity
    Added and learnt:
    Input Actions
    adding relative forces,
    adding rotation
    tuning the rb

---
+++ Rocket Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotaion;
+    [SerializeField] private float thrustStrength = 1000f;
+    [SerializeField] private float rotationStrength = 10f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        thrust.Enable();
+        rotaion.Enable();
+    }
+    private void FixedUpdate()
+    {
+        ProssesThrust();
+        ProssesRotation();
+    }
+
+    private void ProssesThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProssesRotation()
+    {
+        float rotationInput = rotaion.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        Debug.Log("rotacja");
+        rb.freezeRotation = false; // Allow manual control of rotation
+        rb.AddRelativeTorque(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = true;
+    }
+}
